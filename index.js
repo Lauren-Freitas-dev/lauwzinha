4d4ef7eada1a2604ea520f1ec35d7f72c8fc04b9
Lauren-Freitas-dev <laurenfr2015@gmail.com>
2026-07-06 01:37:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -74,20 +74,25 @@ app.post('/mensagens', async (req, res) => {
     }
 });
 
-app.get('/desenhos', async (req, res) => {
-    try {
-        const desenhos = await db.collection('desenhos').find().toArray();
-        res.json(desenhos);
-    } catch (e) {
-        res.status(500).send("Erro ao buscar desenhos");
-    }
-});
-
 app.post('/desenhos', async (req, res) => {
     try {
         const { imagem } = req.body;
+        const ip = req.ip;
+      
+        const umaSemanaAtras = new Date();
+        umaSemanaAtras.setDate(umaSemanaAtras.getDate() - 7);
+
+        const jaDesenhou = await db.collection('desenhos').findOne({
+            ip: ip,
+            data: { $gt: umaSemanaAtras }
+        });
+
+        if (jaDesenhou) {
+            return res.send('Você já enviou um desenho essa semana! Volte em breve 💗');
+        }
 
         await db.collection('desenhos').insertOne({
+            ip: ip,
             imagem: imagem,
             data: new Date()
         });
