b4c459592a98f8278e16c1ca89103302adaf6ab7
Lauren-Freitas-dev <laurenfr2015@gmail.com>
2026-07-05 21:58:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ app.post('/desenhos', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}!`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
 });
 
-conectarDB();
+conectarDB().catch(err => console.error("Erro ao conectar no banco:", err));
